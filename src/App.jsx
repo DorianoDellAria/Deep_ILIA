ffd5c1641a899a877fb3c1ccee77865441dc10d1
lionelGoffaux <lionelg@outlook.be>
2022-03-28 16:53:09 +0200

    update reac router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
 import MainPage from './pages/MainPage'
 
 import './App.css'
 
 function App() {
-
   return (
     <div className='App'>
       <Router>
-        <Switch>
-          <Route path='/' exact render={props => <MainPage {...props} />} />
-
-        </Switch>
+        <Routes>
+          <Route path='/' element={<MainPage/>} />
+        </Routes>
       </Router>
     </div>
   )
