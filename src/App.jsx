a5efd8705cca9cf4aedc5672614de6c2c0e94fc5
DorianoDellAria <doridellaria@live.be>
2022-03-29 21:48:35 +0200

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import NavBar from './components/NavBar'
 
 import MainPage from './pages/MainPage'
 
@@ -8,6 +9,7 @@ import './App.scss'
 function App() {
   return (<>
       <div className='App'>
+        <NavBar />
         <Router>
           <Routes>
             <Route path='/' element={<MainPage/>} />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,9 +3,65 @@ import React from 'react'
 
 function NavBar() {
   return (
-    <button className='button is-primary'>
-      Sign up &gt;
-    </button>
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      {/* <div className="navbar-brand">
+        <a className="navbar-item" href="https://bulma.io">
+          <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+        </a>
+
+        <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div> */}
+
+      <div id="navbarBasicExample" className="navbar-menu">
+        <div className="navbar-start">
+          <a className="navbar-item">
+            Home
+          </a>
+          <a className="navbar-item">
+            Team
+          </a>
+          <a className="navbar-item">
+            Projects
+          </a>
+          <a className="navbar-item">
+            Publications
+          </a>
+          <a className="navbar-item">
+            Applications
+          </a>
+          <a className="navbar-item">
+            Demos
+          </a>
+          <a className="navbar-item">
+            Events
+          </a>
+          <a className="navbar-item">
+            News
+          </a>
+          <a className="navbar-item">
+            
+          </a>
+
+        </div>
+
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="buttons">
+              <a className="button is-primary">
+                Sign up
+              </a>
+              <a className="button is-secondary">
+                Log in
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
   )
 }
 
