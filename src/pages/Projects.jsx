37113b0c7be3f18e0b197224a1e606ef3a3eb04a
lionelGoffaux <lionelg@outlook.be>
2022-03-30 22:52:29 +0200

    prokect image

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -52,6 +52,9 @@ function Projects() {
             01 / 2020 - In Progress
           </div>
         </div>
+        <div className="project-image">
+          <img src="./src/assets/projectillust.png" alt="project"  />
+        </div>
         <p>
           bekijken van de layout van een pagina, afgeleid wordt door de tekstuele inhoud. Het belangrijke punt van het gebruik van Lorem Ipsum is dat het uit een min of meer normale verdeling van letters bestaat, in tegenstelling tot "Hier uw tekst, hier uw tekst" wat het tot min of meer leesbaar nederlands maakt. Veel desktop publishing pakketten en web pagina editors gebruiken tegenwoordig Lorem Ipsum als hun standaard model tekst, en een zoekopdracht naar "lorem ipsum" ontsluit veel websites die nog in aanbouw zijn. Verscheidene versies hebben zich ontwikkeld in de loop van de jaren, soms per ongeluk soms expres (ingevoegde humor en dergelijke).
         </p>
