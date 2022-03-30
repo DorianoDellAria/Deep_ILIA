29bf55085d110cd878beafb79e2aa16cc87980e0
DorianoDellAria <doridellaria@live.be>
2022-03-30 12:16:56 +0200

    wrapping navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import logo from '../assets/deep-logo.svg'
 
 
 function NavBar() {
   return (
     <nav className="navbar" role="navigation" aria-label="main navigation">
-      {/* <div className="navbar-brand">
-        <a className="navbar-item" href="https://bulma.io">
-          <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+      <div className="navbar-brand">
+        <a className="navbar-item" href="#">
+          <img src={logo} width="112" height="28" />
         </a>
 
         <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
@@ -14,7 +15,7 @@ function NavBar() {
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
-      </div> */}
+      </div>
 
       <div id="navbarBasicExample" className="navbar-menu">
         <div className="navbar-start">
@@ -31,10 +32,7 @@ function NavBar() {
             Publications
           </a>
           <a className="navbar-item">
-            Applications
-          </a>
-          <a className="navbar-item">
-            Demos
+            Applications &amp; Demos
           </a>
           <a className="navbar-item">
             Events
@@ -43,7 +41,7 @@ function NavBar() {
             News
           </a>
           <a className="navbar-item">
-            
+
           </a>
 
         </div>
