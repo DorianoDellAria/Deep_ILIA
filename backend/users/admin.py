820ac137c070e4e93039bc179e8eaeb77ab3e09f
DorianoDellAria <doridellaria@live.be>
2022-05-24 10:22:21 +0200

    reset password

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 from .models import User
+from .forms import CustomUserCreationForm
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 
-admin.site.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    add_form = CustomUserCreationForm
+    fieldsets = (
+        *UserAdmin.fieldsets,
+        (
+            'Additional Information',
+            {
+                'fields': (
+                    'Summary',
+                    'Biography',
+                )
+            }
+        )
+    )
+
+admin.site.register(User, CustomUserAdmin)
