f2ee17327e78bca8886064c9a97a0b5a8e34041c
DorianoDellAria <doridellaria@live.be>
2022-05-28 11:08:06 +0200

    fix start db

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,8 +2,12 @@
 
 trap 'trap - TERM; kill -s TERM -- -$$' TERM
 
-python manage.py makemigrations
+python manage.py makemigrations users
 python manage.py migrate
+if [[ "$?" != 0 ]]; then
+    echo "Migration failed"
+    exit 1
+fi
 python add_group.py
 python manage.py crontab add
 gunicorn --bind :8000 backend.wsgi:application &
